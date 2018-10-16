53d5eeaed7a56b8b8789bf34a1b9e6e36a55c9c7
Jakub Kolodziejczyk <jakub.kolodziejczyk@mail.com>
2018-10-17 07:22:10 +0900

    Stubbed out course notes page
    For now it's just a header and profile

---
+++ course_notes/run_server.py
@@ -0,0 +1,20 @@
+"""
+Module to run the server
+"""
+
+import flask
+
+
+APP = flask.Flask("deep_learning_workshop")
+
+
+@APP.route("/")
+def render_index():
+
+    return flask.render_template("index.html")
+
+
+if __name__ == "__main__":
+
+    # To prevent loading model twice, set use_reloader to False
+    APP.run(host="0.0.0.0", use_reloader=True, extra_files=["./templates/index.html", "./templates/macros.html"])
