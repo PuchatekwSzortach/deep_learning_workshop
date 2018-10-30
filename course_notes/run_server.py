214bddeef94b93e451fb23ad8d6fff93bfd7a24e
Jakub Kolodziejczyk <jakub.kolodziejczyk@mail.com>
2018-10-31 08:20:21 +0900

    Added some matrix computations formulas
    mathjax for the win ^^

--- course_notes/run_server.py
+++ course_notes/run_server.py
@@ -17,4 +17,5 @@ def render_index():
 if __name__ == "__main__":
 
     # To prevent loading model twice, set use_reloader to False
-    APP.run(host="0.0.0.0", use_reloader=True, extra_files=["./templates/index.html", "./templates/macros.html"])
+    APP.run(host="0.0.0.0", use_reloader=True,
+            extra_files=["./templates/math_macros.html", "./templates/macros.html", "./templates/index.html"])
