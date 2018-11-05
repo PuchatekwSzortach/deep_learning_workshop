a6eebe07ab4af2c2782f69701e96280da100e52f
Jakub Kolodziejczyk <jakub.kolodziejczyk@mail.com>
2018-11-06 07:25:44 +0900

    Refactored course notes
    Moved some html to sections_macros.html

--- course_notes/run_server.py
+++ course_notes/run_server.py
@@ -18,4 +18,6 @@ if __name__ == "__main__":
 
     # To prevent loading model twice, set use_reloader to False
     APP.run(host="0.0.0.0", use_reloader=True,
-            extra_files=["./templates/math_macros.html", "./templates/macros.html", "./templates/index.html"])
+            extra_files=[
+                "./templates/math_macros.html", "./templates/sections_macros.html",
+                "./templates/chapters_macros.html", "./templates/index.html"])
