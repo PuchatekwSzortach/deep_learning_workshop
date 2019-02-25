bd1f8041325f7d7030e69edaa9555ee5ef36c5b3
Jakub Kolodziejczyk <jakub.kolodziejczyk@mail.com>
2019-02-26 00:13:49 +0900

    Stubbed out convolutional layers field of view page
    Up to one layer computations

--- course_notes/application.py
+++ course_notes/application.py
@@ -10,7 +10,11 @@ def get_app():
     app = flask.Flask("deep_learning_workshop")
 
     @app.route("/deeplearningworkshop.html")
-    def render_index():
+    def render_index_page():
         return flask.render_template("index.html")
+
+    @app.route("/convolutional_layers_field_of_view.html")
+    def render_convolutional_layers_field_of_view_page():
+        return flask.render_template("convolutional_layers_field_of_view/index.html")
 
     return app
--- course_notes/run_server.py
+++ course_notes/run_server.py
@@ -2,6 +2,8 @@
 Module to run the server
 """
 
+import glob
+
 import application
 
 
@@ -11,6 +13,4 @@ if __name__ == "__main__":
 
     # To prevent loading model twice, set use_reloader to False
     app.run(host="0.0.0.0", use_reloader=True,
-            extra_files=[
-                "./templates/math_macros.html", "./templates/sections_macros.html",
-                "./templates/chapters_macros.html", "./templates/index.html"])
+            extra_files=glob.glob("./templates/**/*.html", recursive=True))
